88e27c8293babf5b07933177adc1f76b32edde9d
James <james@ec0.io>
2019-01-15 17:57:47 +1100

    Fixed unittest for reconfigure action

--- src/tests/unit/test_actions.py
+++ src/tests/unit/test_actions.py
@@ -1,12 +1,11 @@
 import imp
-
 import mock
 
 
 class TestActions():
-    def test_example_action(self, libgitlab, monkeypatch):
+    def test_reconfigure_action(self, libgitlab, monkeypatch):
         mock_function = mock.Mock()
-        monkeypatch.setattr(libgitlab, 'action_function', mock_function)
+        monkeypatch.setattr(libgitlab, 'configure', mock_function)
         assert mock_function.call_count == 0
-        imp.load_source('action_function', './actions/example-action')
+        imp.load_source('configure', './actions/reconfigure')
         assert mock_function.call_count == 1
