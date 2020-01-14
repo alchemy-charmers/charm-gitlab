f86a7224ce23e703b27ec58bd1b9ec3adb7f67f8
Chris Sanders <sanders.chris@gmail.com>
2020-01-13 21:52:53 -0600

    Add config option to register IP with proxy

--- lib/libgitlab.py
+++ lib/libgitlab.py
@@ -89,11 +89,17 @@ class GitlabHelper:
         else:
             port = 80
 
+        if self.charm_config["proxy_via_ip"]:
+            networks = hookenv.network_get(proxy.relation_name)
+            internal_host = networks["ingress-addresses"][0]
+        else:
+            internal_host = socket.getfqdn()
+
         proxy_config = [
             {
                 "mode": "http",
                 "external_port": port,
-                "internal_host": socket.getfqdn(),
+                "internal_host": internal_host,
                 "internal_port": self.charm_config["http_port"],
                 "subdomain": url.hostname,
             },
